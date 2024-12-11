bad4f850d3ceb570bcc3a4f2967df36e85f7e439
themark712 <93409384+themark712@users.noreply.github.com>
2024-12-11 16:23:54 -0600

    Add files via upload

---
+++ Chapter02/Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// An unsigned integer is a positive whole number or 0
+uint naturalNumber = 23;
+
+// An integer is a negative or positive whole number or 0
+int integerNumber = -23;
+
+// A float is a single-precision floating-point number
+// The F or f suffix makes the value a float literal
+// The suffix is required to compile
+float realNumber = 2.3f;
+
+// A double is a double-precision floating-point number
+// double is the default for a number value with a decimal point
+double anotherRealNumber = 2.3;     // A double literal value
+
+int decimalNotation = 2_000_000;
+int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
+int hexadecimalNotation = 0x_001E_8480;
+
+// Check the three variable have the same value
+Console.WriteLine($"{decimalNotation == binaryNotation}");
+Console.WriteLine($"{decimalNotation == hexadecimalNotation}");
+
+// Output the  variable values in decimal
+Console.WriteLine($"{decimalNotation:N0}");
+Console.WriteLine($"{binaryNotation:N0}");
+Console.WriteLine($"{hexadecimalNotation:N0}");
+
+// Output the variable values in hexadecimal
+Console.WriteLine($"{decimalNotation:X}");
+Console.WriteLine($"{binaryNotation:X}");
+Console.WriteLine($"{hexadecimalNotation:X}");
---
+++ Chapter02/Vocabulary/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection;
+
+// Declare some unused variable using types in
+// additional assemblies to make them load too
+System.Data.DataSet ds = new();
+HttpClient client = new();
+
+// Get the assembly that is the entry point for this app
+Assembly? myApp = Assembly.GetEntryAssembly();
+
+// If the previous line returned nothing then end thte app
+if (myApp is null) return;
+
+// Loop through the assemblies that my app references
+foreach (AssemblyName name in myApp.GetReferencedAssemblies())
+{
+  // Load the assembly so we can read its details
+  Assembly a = Assembly.Load(name);
+
+  // Declare a variable to count the number of methods
+  int methodCount = 0;
+  
+  // Loop through all the types in the assembly
+  foreach(TypeInfo t in a.DefinedTypes) {
+    // Add up the counts of all the methods
+    methodCount += t.GetMethods().Length;
+  }
+
+  // Output the count of types and their methods
+  Console.WriteLine("{0:N0} types with {1:N0} methods in {2} assembly.", arg0: a.DefinedTypes.Count(), arg1: methodCount, arg2:name.Name);
+
+  Console.OutputEncoding = System.Text.Encoding.UTF8;
+  string grinningEmoji = char.ConvertFromUtf32(0x1F600);
+  Console.WriteLine(grinningEmoji);
+}
